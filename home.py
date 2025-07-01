532c75b9b99eaed420e87794af13fdcedab2638b
pooja-369 <pujagoyal369@gmail.com>
2025-07-01 12:37:41 +0530

    #

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to my project👋")
+
+st.write("Your price prediction UI goes here.")
+
+
+st.sidebar.success("Select a demo above.")
